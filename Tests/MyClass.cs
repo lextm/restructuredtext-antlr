3ae70b47b0b1a09192a0ab29264fbccf4e323780
Lex Li <support@lextm.com>
2017-12-15 00:35:18 -0500

    Added first unit test case.

---
+++ Tests/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using ReStructuredText;
+using Xunit;
+
+namespace Tests
+{
+    public class MyClass
+    {
+        [Fact]
+        public void Test()
+        {
+            var content = "I test";
+            var path = Path.GetTempFileName();
+            File.WriteAllText(path, content);
+
+            restructuredtextParser.ParseDocument(path);
+            Assert.False(true);
+        }
+    }
+}
