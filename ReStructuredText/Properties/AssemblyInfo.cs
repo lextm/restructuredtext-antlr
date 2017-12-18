b4106be699e6dc55e6e20edc9dcbec6edee85178
Lex Li <support@lextm.com>
2017-12-18 18:15:32 -0500

    Converted to a .NET Standard project.

--- ReStructuredText/Properties/AssemblyInfo.cs
+++ ReStructuredText/Properties/AssemblyInfo.cs
@@ -24,12 +24,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReStructuredText for .NET")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ReStructuredText for .NET")]
-[assembly: AssemblyCopyright("Copyright © 2011 Bart Kiers. Copyright © 2017 Lex Li.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -40,18 +34,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a7e5ad0b-65f6-4c42-af05-b243ff5c4876")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: CLSCompliant(true)]
